53ebad56fda378e0a66c728929d58208eb7b6c1e
suhailkassar11 <suhail.1923co1011@kiet.edu>
2026-06-02 05:34:53 -0400

    change mount logic in child

--- internal/runtime/process.go
+++ internal/runtime/process.go
@@ -10,7 +10,7 @@ import (
 func Run(command []string) {
 	args := append([]string{"child"}, command...)
 	cmd := exec.Command("/proc/self/exe", args...)
-	cmd.SysProcAttr=&syscall.SysProcAttr{
+	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS,
 	}
 	cmd.Stdin = os.Stdin
@@ -25,24 +25,16 @@ func Run(command []string) {
 }
 
 func Child(command []string) {
-	
-	cmd := exec.Command(command[0], command[1:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
 
 	fmt.Println("CHILD")
 
-    syscall.Sethostname([]byte("tiny-docker"))
+	syscall.Sethostname([]byte("tiny-docker"))
 
-    syscall.Mount("proc", "/proc", "proc", 0, "")
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
 
-    if err := syscall.Exec(command[0], command, os.Environ()); err != nil {
-        panic(err)
-    }
-	
-	err:=cmd.Run()
-	if err!=nil{
+	syscall.Mount("proc", "/proc", "proc", 0, "")
+
+	if err := syscall.Exec(command[0], command, os.Environ()); err != nil {
 		panic(err)
 	}
 }
